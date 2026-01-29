d4d2639bb0df55cb8df6462c1cc79ba9073a979a
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2026-01-29 00:04:17 -0800

    Update bindings.h

--- bindings.h
+++ bindings.h
@@ -1,3 +1,4 @@
+// Add esp camera module
 // https://docs.esp-rs.org/esp-idf-hal/esp_idf_sys/index.html#remote-components-idf-component-registry
 #if defined(ESP_IDF_COMP_ESPRESSIF__ESP32_CAMERA_ENABLED)
 #include "esp_camera.h"
