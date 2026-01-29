00c2c1c299fc182f5890a4bf602688c33aaa9e2e
tmpk <>
2026-01-28 23:52:15 -0800

    Adding comments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use esp_camera_rs;
-use esp_idf_hal;
-use esp_idf_sys::camera::framesize_t_FRAMESIZE_240X240;
-use rxing::BarcodeFormat;
+/*
+
+QR code detection and decoding
+
+Using the rxing, esp-idf, and esp32-camera-rs crates 
+On the Xiao esp32s3 sense w/ camera
+
+*/
 
 fn main() {
+    // Esp-idf setup
+
+    // It is necessary to call this function once. Otherwise, some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+
+    // Get esp32s3 pins
+    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
+
     // https://wiki.seeedstudio.com/xiao_esp32s3_getting_started/#hardware-overview
-    /*
+    /* Camera
     Chip Pin	Description
     GPIO10	Camera-related clock pin
     GPIO11	Camera video data pin (Y8)
@@ -22,11 +39,11 @@ fn main() {
     GPIO47	Camera horizontal sync pin
     GPIO48	Camera video data pin (Y9)
     */
-    let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
+    // Setup xiao esp32s3 pins for camera
     let camera = esp_camera_rs::Camera::new(
         peripherals.pins.gpio1,  // Unused
         peripherals.pins.gpio2,  // Unused
-        peripherals.pins.gpio10, // "Camera related clock pin"
+        peripherals.pins.gpio10, // gpio10 "Camera related clock pin"
         peripherals.pins.gpio15, // gpio15 pin_d0 Y2
         peripherals.pins.gpio17, // gpio17 pin_d1 Y3
         peripherals.pins.gpio18, // gpio18 pin_d2 Y4
@@ -43,25 +60,38 @@ fn main() {
     )
     .unwrap();
 
+    // Set framesize to 240x240
     camera
         .sensor()
-        .set_framesize(framesize_t_FRAMESIZE_240X240)
+        .set_framesize(esp_idf_sys::camera::framesize_t_FRAMESIZE_240X240)
         .unwrap();
 
-    let fb = camera.get_framebuffer().unwrap();
+    fn detect(camera: &esp_camera_rs::Camera) {
+        // Get frame buffer from camera
+        let frame_buffer = camera.get_framebuffer().unwrap();
 
-    let code =
-        rxing::helpers::detect_in_luma(fb.data().to_vec(), 240, 240, Some(BarcodeFormat::QR_CODE));
+        // Attempt to detect/decode QR from framebuffer
+        let qrcode = rxing::helpers::detect_in_luma(
+            frame_buffer.data().to_vec(),
+            240,
+            240,
+            Some(rxing::BarcodeFormat::QR_CODE),
+        );
 
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+        // Handel "No qrcode found"
+        match qrcode {
+            Ok(c) => log::info!("QRcode: {}", c.getText()),
+            Err(e) => log::info!("No qrcode found {}", e),
+        }
+    }
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
-
-    match code {
-        Ok(c) => log::info!("QRcode: {}", c.getText()),
-        Err(e) => log::info!("No qrcode found {}", e),
+    // If loop feature is enabled attempt to detect every 10s
+    if cfg!(feature = "loop") {
+        loop {
+            detect(&camera);
+            esp_idf_hal::delay::Delay::delay_ms(&esp_idf_hal::delay::Delay::default(), 10_000);
+        }
+    } else {
+        detect(&camera);
     }
 }
